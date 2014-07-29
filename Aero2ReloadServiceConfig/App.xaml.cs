e5c8bbee18a3778514bcbbf760f20cbbb365e206
Dawid Cieszynski <dawid.cieszynski@crystalcode.pl>
2014-07-29 19:26:54 +0200

    uruchamianie z uprawnieniami administratora

--- Aero2ReloadServiceConfig/App.xaml.cs
+++ Aero2ReloadServiceConfig/App.xaml.cs
@@ -1,5 +1,9 @@
 ﻿namespace Aero2ReloadServiceConfig
 {
+    using System;
+    using System.Diagnostics;
+    using System.Reflection;
+    using System.Security.Principal;
     using System.Windows;
 
     /// <summary>
@@ -7,5 +11,42 @@
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (!this.IsRunAsAdministrator())
+            {
+                this.RunThisAsAdministrator();
+                this.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        private void RunThisAsAdministrator()
+        {
+            var processInfo = new ProcessStartInfo(Assembly.GetExecutingAssembly().CodeBase)
+            {
+                UseShellExecute = true,
+                Verb = "runas"
+            };
+
+            try
+            {
+                Process.Start(processInfo);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Aplikacja musi być uruchomiona z uprawnieniami administratora.");
+            }
+        }
+
+        private bool IsRunAsAdministrator()
+        {
+            var wi = WindowsIdentity.GetCurrent();
+            var wp = new WindowsPrincipal(wi);
+
+            return wp.IsInRole(WindowsBuiltInRole.Administrator);
+        }
     }
 }
